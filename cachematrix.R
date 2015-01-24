258e5bacf298bf34090bca49245e7cae7c79eb61
YaRKiY <1yarkiyrnd1@gmail.com>
2015-01-24 19:10:47 +0300

    R script for Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes any matrix that we asked and
+##pushes it into another environment
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## First, we give the function object
+##which we did in the previous function. 
+##Then, this function checks whether the inverse matrix 
+##in the cache. If it finds one, it returns stored data 
+## If not, invert the matrix, and stores it 
+##in the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}     
